981e7ac94897b202abeb23a2cf4285a71051fbd7
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-01-02 12:02:10 +0100

    sample

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,38 @@
+import androidx.compose.desktop.AppWindowAmbient
 import androidx.compose.desktop.Window
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.getValue
+import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.IntOffset
 
-fun main() = Window {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun main() {
+    val windowPos = mutableStateOf(IntOffset.Zero)
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    Window {
+        val current = AppWindowAmbient.current
+
+        // Content
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column {
+                Text(text = "Location: ${windowPos.value}")
+                Button(
+                    onClick = {
+                        if (current != null) {
+                            windowPos.value = IntOffset(current.x, current.y)
+                        }
+                    }
+                ) {
+                    Text(text = "Print window location")
+                }
+            }
         }
     }
 }
